f6035ea858217a395cdaffa99062394d897ceba9
typhlosiongo <professionaloscar@gmail.com>
2019-09-16 20:47:30 -0700

    Initial Commit

---
+++ Tippo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tippo
+//
+//  Created by Oscar Ramirez on 9/16/19.
+//  Copyright © 2019 Oscar Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
